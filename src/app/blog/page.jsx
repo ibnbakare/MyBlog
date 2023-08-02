dfd25e1302b65ca1f70850fc9db5d0a268106896
ridhwan <ridmartrite@gmail.com>
2023-08-02 16:56:00 +0100

    blog feature

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import styles from "./page.module.css"
+import Image from 'next/image'
+import Link from 'next/link'
 const Blog = () => {
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+    
+        <Link href={`/blog/me`} className={styles.container}>
           <div className={styles.imageContainer}>
             <Image
-              src={item.img}
+              src="https://images.pexels.com/photos/14039690/pexels-photo-14039690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+
               alt=""
               width={400}
               height={250}
@@ -15,11 +18,28 @@ const Blog = () => {
             />
           </div>
           <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.desc}</p>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
           </div>
         </Link>
-      ))}
+        
+        <Link href={`/blog/me`} className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/14039690/pexels-photo-14039690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+    
     </div>
   )
 }
