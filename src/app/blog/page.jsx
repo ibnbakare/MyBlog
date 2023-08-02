82547179e79c4b82023f2593b813ce0f01a4bea4
ridhwan <ridmartrite@gmail.com>
2023-08-02 16:32:21 +0100

    portfolio cat feature

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
-const Portfoilocategory = ({category}) => {
+import styles from "./page.module.css"
+import Button from '@/component/button/button'
+import Image from 'next/image'
+const Portfoilocategory = ({params}) => {
+  console.log(params)
+  const{category} = params
   return (
-    <div>Portfoilocategory{category}</div>
+   
+    <div className={styles.container}>
+      <h2 className={styles.title}>{category}</h2>
+      <div className={styles.items}> 
+        <div className={styles.imgContent}>
+          <Image src ="https://images.pexels.com/photos/17807349/pexels-photo-17807349/free-photo-of-art-architecture-travel-statue.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            fill={true}
+            alt=''
+            className={styles.img}
+         />
+        </div>
+        <div className={styles.textContent}>
+          <h2 className={styles.contentTitle}>Creative minds</h2>
+          <p className={styles.desc}>Desc</p>
+          <Button title="See More" url = "#" />
+        </div>
+        
+      </div>
+      
+      <div className={styles.items}> 
+        <div className={styles.imgContent}>
+          <Image src ="https://images.pexels.com/photos/17807349/pexels-photo-17807349/free-photo-of-art-architecture-travel-statue.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            fill={true}
+            alt=''
+            className={styles.img}
+         />
+        </div>
+        <div className={styles.textContent}>
+          <h2 className={styles.contentTitle}>Creative minds</h2>
+          <p className={styles.desc}>Desc</p>
+          <Button title="See More" url = "#" />
+        </div>
+      </div>
+    </div>
   )
 }
 
