8fb656fb57d7298c5e24dffb1a664412f4d4e632
ridhwan <ridmartrite@gmail.com>
2023-08-04 18:33:25 +0100

    Done with light and dark mode toggle feature

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import Navbar from '@/component/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/component/footer/Footer'
+import { ThemeProvider } from '@/context/ThemeContext'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,11 +17,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
    
       <body className={inter.className}>
+      <ThemeProvider>
       <div className='container'>
        <Navbar />
         {children}
        <Footer />
+      
         </div>
+        </ThemeProvider>
         </body>
       
     </html>
--- src/component/darktoggle/Darktoggleicon.jsx
+++ src/component/darktoggle/Darktoggleicon.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+"use client"
+
+import React, { useContext } from 'react'
 import styles from "./DarkToggle.module.css"
+import { ThemeContext } from '@/context/ThemeContext'
+
 
 const Darktoggleicon = () => {
+// const mode = "dark"
+const {toggle,mode} = useContext(ThemeContext)
   return (
-    <div className={styles.container} >
+    <div className={styles.container} onClick={toggle} >
     <div className={styles.icon}>M</div>
     <div className={styles.icon}>S</div>
-    <div className={styles.ball}></div>
+    <div className={styles.ball} style={mode ==="light"? {left:"2px"} : {right:"2px"}} />
 </div>
   )
 }
