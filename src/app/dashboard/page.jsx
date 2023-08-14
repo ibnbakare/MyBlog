f60a75ff9f45f32cc3dab32afec65963c7a872c8
ridhwan <ridmartrite@gmail.com>
2023-08-14 13:44:22 +0100

    creating Oauth credientials

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import useSWR from "swr"
 import styles from "./page.module.css"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Dashboard = () => {
   // const [data, setData] = useState([])
@@ -26,6 +27,8 @@ const Dashboard = () => {
   //     }
   //     getData()
   //   }, [])
+  const session = useSession()
+  console.log(session)
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts/", fetcher)
     console.log(data)
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/component/footer/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
+import AuthProvider from '@/component/AuthProvider/AuthProvider'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,12 +19,14 @@ export default function RootLayout({ children }) {
    
       <body className={inter.className}>
       <ThemeProvider>
+        <AuthProvider >
       <div className='container'>
        <Navbar />
         {children}
        <Footer />
       
         </div>
+        </AuthProvider>
         </ThemeProvider>
         </body>
       
