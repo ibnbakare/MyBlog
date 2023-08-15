a1bf15cdce0958cda994c88fc174fdddf804dfe8
ridhwan <ridmartrite@gmail.com>
2023-08-15 17:22:08 +0100

    creating login page with credentails enabled

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
+import connect from "@/utils/db"
+import User from "@/models/User"
+import bcrypt from "bcryptjs"
 
 const handler = NextAuth({
   providers: [
@@ -10,7 +13,29 @@ const handler = NextAuth({
     }),
 
     CredentialsProvider({
-      name:Credential
+      name:Credentials,
+      id:credentials,
+      async authorized(credentials){
+        await connect()
+        try {
+          const user = User.findOne({email:credentials.email})
+          if(user){
+            const isPasswordValid = await bcrypt.compare(
+              credentials.password,user.password)
+          
+          if(isPasswordValid){
+            return user
+          }
+          else{
+            throw new Error("Wrong email or password")
+          }
+        }else{
+          throw new Error("User not found")
+        }
+        } catch (error) {
+          throw new Error(error)
+        }
+      }
     }
 
     )
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -5,6 +5,30 @@ import React from 'react'
 const page = () => {
   return (
     <div>
+       <div >
+        <form className={styles.container} onSubmit={handleForm}>
+            <input type='text' 
+            required
+            placeholder='username'
+            className={styles.input}
+            />
+            <input type='text' 
+            required
+            placeholder='email'
+            className={styles.input}
+            />
+            <input type='password' 
+            required
+            placeholder='password'
+            className={styles.input}
+            
+            />
+            <button className={styles.button} > Register</button>
+            
+        </form>
+        {err && "Something Went Wrong"}
+        <Link href="/login" > Click here if registered</Link>
+    </div>
         <button onClick={() => signIn("google")} >Sign in With Google</button>
     </div>
 
